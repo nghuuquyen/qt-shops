3612dfb83f323ccfea79e5d72a469a6d97a2cb11
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2023-05-30 05:53:47 +0000

    Improve loading theme and locale for better Ux

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,30 +1,46 @@
+@php
+    use \Illuminate\Support\Facades\Session;
+    use Illuminate\Support\Facades\App;
+
+    $theme = Session::get('theme', 'auto');
+
+    $locale = App::currentLocale();
+
+    $bg_backgroud = 'bg-background';
+
+    if ($theme == 'theme-light') {
+        $bg_backgroud = 'bg-gradient-to-r from-rose-100 to-teal-100';
+    }
+@endphp
+
 <!DOCTYPE html>
-<html lang="en" class="theme-dark">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $page_title ?? 'Coffee Shops' }}</title>
+<html lang="{{ $locale }}" class="{{ $theme }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{ $page_title ?? 'Coffee Shops' }}</title>
 
-    @livewireStyles
-    @vite('resources/css/app.css')
-</head>
-<body class="bg-background">
-    <x-logo />
+        @livewireStyles
+        @vite('resources/css/app.css')
+    </head>
 
-    @yield('navigation')
+    <body class="{{ $bg_backgroud }}">
+        <x-logo />
 
-    <main class="max-w-screen-xl m-auto grid grid-cols-1 gap-10 mt-6 lg:p-10 p-4">
-        @yield('main')
-    </main>
-    
-    @yield('components')
+        @yield('navigation')
 
-    <script>
-        window.theme = '{{ \Illuminate\Support\Facades\Session::get('theme', 'auto') }}';
-    </script>
+        <main class="max-w-screen-xl m-auto grid grid-cols-1 gap-10 mt-6 lg:p-10 p-4">
+            @yield('main')
+        </main>
+        
+        @yield('components')
 
-    @livewireScripts
-    @vite('resources/js/app.js')
-</body>
+        <script>
+            window.theme = '{{ $theme }}';
+        </script>
+
+        @livewireScripts
+        @vite('resources/js/app.js')
+    </body>
 </html>
