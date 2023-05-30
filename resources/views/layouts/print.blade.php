5d3df4ea8ce117fbd5408afddcaa3cb9911b65b4
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2023-05-30 06:48:10 +0000

    Improve print format page

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -26,9 +26,9 @@
     </head>
 
     <body class="{{ $bg_backgroud }}">
-        <x-logo :hasSetting="false" />
+        <x-logo :hasSetting="false" class="bg-transparent pt-6"/>
 
-        <main class="max-w-screen-xl m-auto grid grid-cols-1 gap-10 mt-6 lg:p-10 p-4">
+        <main class="max-w-screen-xl m-auto grid grid-cols-1 gap-10 p-4 lg:p-10">
             @yield('main')
         </main>
         
